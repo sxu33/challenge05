059b4026ce06cf76470fd00865d2ea8f16996be7
sxu <sxu@szx>
2024-06-11 21:27:42 -0500

    finish

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,3 @@
 class Product < ApplicationRecord
-    validates :title, presence: true
-    validates :price, presence: true
-    validates :stock_quantity, presence: true
+    belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-require 'faker'
+require 'csv'
 
-676.times do
+Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |product|
+  category_name = product['category']
+  category = Category.find_or_create_by(name: category_name)
+
   Product.create(
-    title: Faker::Commerce.product_name,
-    price: Faker::Commerce.price,
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+    title: product['name'],
+    description: product['description'],
+    price: product['price'],
+    stock_quantity: product['stock quantity'],
+    category: category
   )
 end
